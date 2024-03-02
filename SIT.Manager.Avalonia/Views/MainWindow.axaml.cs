8f18957adfe8e1c56d6d25e9669146002e60480d
J0nathan550 <ihudokormov@gmail.com>
2024-03-02 18:55:42 +0100

    Changed TitleBar for the launcher and accent color.

--- SIT.Manager.Avalonia/Views/MainWindow.axaml.cs
+++ SIT.Manager.Avalonia/Views/MainWindow.axaml.cs
@@ -1,14 +1,27 @@
-﻿using Avalonia.Controls;
+﻿using FluentAvalonia.UI.Windowing;
 using Microsoft.Extensions.DependencyInjection;
 using SIT.Manager.Avalonia.ManagedProcess;
+using Avalonia.Media;
 using System;
 
 namespace SIT.Manager.Avalonia.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : AppWindow
 {
     public MainWindow() {
         InitializeComponent();
+        TitleBar.BackgroundColor = new Color(0xFF, 0x00, 0x00, 0x00);
+        TitleBar.ForegroundColor = new Color(0xFF, 0xFF, 0xFF, 0xFF);
+        TitleBar.InactiveBackgroundColor = new Color(0xFF, 0x00, 0x00, 0x00);
+        TitleBar.InactiveForegroundColor = new Color(0xFF, 0xFF, 0xFF, 0xFF);
+        TitleBar.ButtonBackgroundColor = new Color(0xFF, 0x00, 0x00, 0x00);
+        TitleBar.ButtonForegroundColor = new Color(0xFF, 0xFF, 0xFF, 0xFF);
+        TitleBar.ButtonHoverBackgroundColor = new Color(0xFF, 0x11, 0x11, 0x11);
+        TitleBar.ButtonHoverForegroundColor = new Color(0xFF, 0xFF, 0xFF, 0xFF);
+        TitleBar.ButtonPressedBackgroundColor = new Color(0xFF, 0x21, 0x21, 0x21);
+        TitleBar.ButtonPressedForegroundColor = new Color(0xFF, 0xFF, 0xFF, 0xFF);
+        TitleBar.ButtonInactiveBackgroundColor = new Color(0xFF, 0x00, 0x00, 0x00);
+        TitleBar.ButtonInactiveForegroundColor = new Color(0xFF, 0xFF, 0xFF, 0xFF);
     }
 
     private void Window_Closed(object? sender, EventArgs e) {
