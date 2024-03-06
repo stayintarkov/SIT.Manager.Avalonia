009466e6cbd24c938c91118a5960a405d10af44f
artehe <112902041+artehe@users.noreply.github.com>
2024-03-06 19:21:52 +0000

    change to use different file extraction method

--- SIT.Manager.Updater/Program.cs
+++ SIT.Manager.Updater/Program.cs
@@ -1,6 +1,8 @@
-﻿using SIT.Manager.Updater;
+﻿using SharpCompress.Archives;
+using SharpCompress.Archives.Zip;
+using SharpCompress.Common;
+using SIT.Manager.Updater;
 using System.Diagnostics;
-using System.IO.Compression;
 
 const string SITMANAGER_PROC_NAME = "SIT.Manager.Avalonia.Desktop.exe";
 const string SITMANAGER_RELEASE_URL = @"https://github.com/stayintarkov/SIT.Manager.Avalonia/releases/download/beta/win-x64.zip";
@@ -23,11 +25,11 @@ if (!skipInteractivity)
 }
 
 Process[] managerProcesses = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(SITMANAGER_PROC_NAME));
-if(managerProcesses.Length > 0)
+if (managerProcesses.Length > 0)
 {
     if (killProcNoPrompt == false)
     {
-        if(skipInteractivity)
+        if (skipInteractivity)
             Environment.Exit(2);
         Console.WriteLine("An instance of 'SIT.Manager.Avalonia' was found. Would you like to close all instances? Y/N");
         string? response = Console.ReadLine();
@@ -39,7 +41,7 @@ if(managerProcesses.Length > 0)
     {
         Console.WriteLine("Killing {0} with PID {1}\n", process.ProcessName, process.Id);
         bool clsMsgSent = process.CloseMainWindow();
-        if(!clsMsgSent || !process.WaitForExit(TimeSpan.FromSeconds(5)))
+        if (!clsMsgSent || !process.WaitForExit(TimeSpan.FromSeconds(5)))
         {
             process.Kill();
         }
@@ -48,7 +50,7 @@ if(managerProcesses.Length > 0)
 
 
 string workingDir = AppDomain.CurrentDomain.BaseDirectory;
-if(!File.Exists(Path.Combine(workingDir, SITMANAGER_PROC_NAME)))
+if (!File.Exists(Path.Combine(workingDir, SITMANAGER_PROC_NAME)))
 {
     Console.WriteLine("Unable to find '{0}' in root directory. Make sure the app is installed correctly.", SITMANAGER_PROC_NAME);
     if (!skipInteractivity)
@@ -65,7 +67,7 @@ string zipName = Path.GetFileName(SITMANAGER_RELEASE_URL);
 string zipPath = Path.Combine(tempPath, zipName);
 
 int progressBarUpdateRate = 30;
-using(CLIProgressBar progressBar = new(progressBarUpdateRate))
+using (CLIProgressBar progressBar = new(progressBarUpdateRate))
 {
     try
     {
@@ -78,7 +80,7 @@ using(CLIProgressBar progressBar = new(progressBarUpdateRate))
     catch (Exception ex)
     {
         Console.WriteLine("Error during download: {0}", ex.Message);
-        if(!skipInteractivity)
+        if (!skipInteractivity)
         {
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
@@ -91,7 +93,7 @@ Console.WriteLine("\nDownload complete.");
 Console.WriteLine("Creating backup of SIT.Manager");
 
 string backupPath = Path.Combine(workingDir, "Backup");
-if(Directory.Exists(backupPath))
+if (Directory.Exists(backupPath))
     Directory.Delete(backupPath, true);
 
 DirectoryInfo workingFolderInfo = new(workingDir);
@@ -104,14 +106,31 @@ Console.WriteLine("\nBackup complete. Extracting new version..\n");
 
 DirectoryInfo releasePath = new(Path.Combine(tempPath, "Release"));
 releasePath.Create();
-ZipFile.ExtractToDirectory(zipPath, releasePath.FullName, true);
+using (ZipArchive archive = ZipArchive.Open(zipPath))
+{
+    foreach (ZipArchiveEntry entry in archive.Entries)
+    {
+        if (entry.IsDirectory)
+        {
+            continue;
+        }
+        else
+        {
+            entry.WriteToDirectory(releasePath.FullName, new ExtractionOptions()
+            {
+                ExtractFullPath = true,
+                Overwrite = true
+            });
+        }
+    }
+}
 
 await releasePath.MoveSIT(workingDir);
 
 Directory.Delete(tempPath, true);
 
 Console.WriteLine($"\nUpdate done. Backup can be found in the {Path.GetFileName(backupPath)} folder. Your settings have been saved.");
-if(!skipInteractivity)
+if (!skipInteractivity)
 {
     Console.WriteLine("Press any key to finish...");
     Console.ReadKey();
