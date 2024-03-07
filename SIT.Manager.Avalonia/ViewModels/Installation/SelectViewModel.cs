7a42f80373ccdd00abe0f1480a86d3d83708347b
artehe <112902041+artehe@users.noreply.github.com>
2024-03-07 15:07:13 +0000

    Create layout for select view

--- SIT.Manager.Avalonia/ViewModels/Installation/SelectViewModel.cs
+++ SIT.Manager.Avalonia/ViewModels/Installation/SelectViewModel.cs
@@ -1,11 +1,17 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using SIT.Manager.Avalonia.ManagedProcess;
+using SIT.Manager.Avalonia.Models;
 using SIT.Manager.Avalonia.Models.Messages;
 
 namespace SIT.Manager.Avalonia.ViewModels.Installation
 {
-    public partial class SelectViewModel : ViewModelBase
+    public partial class SelectViewModel(IManagerConfigService configsService) : ViewModelBase
     {
+        [ObservableProperty]
+        private ManagerConfig _config = configsService.Config;
+
         [RelayCommand]
         private void Progress()
         {
