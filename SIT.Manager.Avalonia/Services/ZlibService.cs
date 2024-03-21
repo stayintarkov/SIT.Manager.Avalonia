ca3d25b4e175b7635295ed07ece5a30743344c2a
Luna <34936608+Plootie@users.noreply.github.com>
2024-03-21 04:26:30 +0000

    Rework zlib service slightly

--- SIT.Manager.Avalonia/Services/ZlibService.cs
+++ SIT.Manager.Avalonia/Services/ZlibService.cs
@@ -13,6 +13,7 @@ public class ZlibService : IZlibService
     private Assembly? _zlibAssembly;
     private MethodInfo? compressToBytesMethodInfo;
     private MethodInfo? decompressMethodInfo;
+
     public ZlibService(IManagerConfigService configService)
     {
         if (string.IsNullOrEmpty(configService.Config.InstallPath))
@@ -22,27 +23,30 @@ public class ZlibService : IZlibService
     }
 
     public byte[] CompressToBytes(string data, ZlibCompression compressionProfile, Encoding? encoding = null)
-    {
-        return (byte[]?) compressToBytesMethodInfo?.Invoke(null, new object[] { data, compressionProfile, encoding ?? Encoding.UTF8 }) ?? [];
-    }
+        => (byte[]?) compressToBytesMethodInfo?.Invoke(null, [data, compressionProfile, encoding ?? Encoding.UTF8]) ?? [];
 
     public string Decompress(byte[] data, Encoding? encoding = null)
-    {
-        return (string?) decompressMethodInfo?.Invoke(null, new object[] { data, encoding ?? Encoding.UTF8 }) ?? string.Empty;
-    }
+        => (string?) decompressMethodInfo?.Invoke(null, [data, encoding ?? Encoding.UTF8]) ?? string.Empty;
 
+    //TODO: Add logging to this method
     private void LoadZlibFromConfig(object? sender, ManagerConfig e)
     {
         if (!string.IsNullOrEmpty(e.InstallPath))
         {
             string assemblyPath = Path.Combine(e.InstallPath, "EscapeFromTarkov_Data", "Managed", "bsg.componentace.compression.libs.zlib.dll");
+            if (!File.Exists(assemblyPath))
+                return;
+
             _zlibAssembly = Assembly.LoadFrom(assemblyPath);
             Type? SimpleZlib = _zlibAssembly?.GetType("ComponentAce.Compression.Libs.zlib.SimpleZlib");
-            compressToBytesMethodInfo = SimpleZlib?.GetMethod("CompressToBytes", [typeof(string), typeof(int), typeof(Encoding)]);
-            decompressMethodInfo = SimpleZlib?.GetMethod("Decompress", [typeof(byte[]), typeof(Encoding)]);
+            if (SimpleZlib != null)
+            {
+                compressToBytesMethodInfo = SimpleZlib?.GetMethod("CompressToBytes", [typeof(string), typeof(int), typeof(Encoding)]);
+                decompressMethodInfo = SimpleZlib?.GetMethod("Decompress", [typeof(byte[]), typeof(Encoding)]);
 
-            if (sender is IManagerConfigService configService && compressToBytesMethodInfo != null && decompressMethodInfo != null)
-                configService.ConfigChanged -= LoadZlibFromConfig;
+                if (sender is IManagerConfigService configService && compressToBytesMethodInfo != null && decompressMethodInfo != null)
+                    configService.ConfigChanged -= LoadZlibFromConfig;
+            }
         }
     }
 }
