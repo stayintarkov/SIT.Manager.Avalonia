54e5bf3eee866d4162b90820136d366731e5611a
Luna <34936608+Plootie@users.noreply.github.com>
2024-03-25 05:56:54 +0000

    Push Initial AkiProfile

---
+++ SIT.Manager.Avalonia/Models/AkiProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIT.Manager.Avalonia.Models;
+public class AkiProfile(string profileID)
+{
+    public string ProfileID { get; } = profileID;
+}
