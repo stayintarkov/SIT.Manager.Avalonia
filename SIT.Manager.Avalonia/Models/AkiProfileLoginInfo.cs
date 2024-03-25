5b7044e7e8b14c9ed5715bf06b57756e678a6997
Luna <34936608+Plootie@users.noreply.github.com>
2024-03-25 06:35:36 +0000

    Create AkiProfileLoginInfo.cs

---
+++ SIT.Manager.Avalonia/Models/AkiProfileLoginInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SIT.Manager.Avalonia.Models;
+public class AkiProfileLoginInfo(AkiProfile akiProfile)
+{
+    [JsonPropertyName("username")]
+    public string Username { get; } = akiProfile.Username;
+    [JsonPropertyName("email")]
+    public string Email => Username;
+    [JsonPropertyName("edition")]
+    public string Edition { get; set; } = akiProfile.Edition;
+    [JsonPropertyName("password")]
+    public string Password { get; init; } = akiProfile.Password;
+    [JsonPropertyName("backendUrl")]
+    public string BackendUrl { get; init; } = akiProfile.Server.Address
+}
