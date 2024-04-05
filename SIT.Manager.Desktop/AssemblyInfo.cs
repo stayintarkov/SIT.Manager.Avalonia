dd3d3914abdc10581b3041c6d2af34d3a015039f
DR <39994013+DRDRDRRDDRDR@users.noreply.github.com>
2024-04-06 02:57:26 +0800

    Add files via upload

---
+++ SIT.Manager.Desktop/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © StayInTarkov 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: SupportedOSPlatform("windows")]
+[assembly: SupportedOSPlatform("linux")]
