00f423f911ec3703a398e9e8dabd4f40aa80135b
DR <39994013+DRDRDRRDDRDR@users.noreply.github.com>
2024-04-06 03:36:07 +0800

    Delete SIT.Manager/AssemblyInfo.cs

--- SIT.Manager/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.Versioning;
-
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCopyright("Copyright © StayInTarkov 2024")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: SupportedOSPlatform("windows")]
-[assembly: SupportedOSPlatform("linux")]
