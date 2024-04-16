d4ee588e3e12dc14b5d0c1b469c4d123cafdaad2
artehe <112902041+artehe@users.noreply.github.com>
2024-04-16 17:59:09 +0100

    oops

--- SIT.Manager/Models/ManagerConfig.cs
+++ SIT.Manager/Models/ManagerConfig.cs
@@ -33,7 +33,7 @@ public partial class ManagerConfig : ObservableObject
     [ObservableProperty]
     public string _sitVersion = string.Empty;
     [ObservableProperty]
-    private DateTime _lastSitUpdateCheckTime = DateTime.MaxValue;
+    private DateTime _lastSitUpdateCheckTime = DateTime.MinValue;
     [ObservableProperty]
     public string _sptAkiVersion = string.Empty;
     [ObservableProperty]
@@ -41,7 +41,7 @@ public partial class ManagerConfig : ObservableObject
     [ObservableProperty]
     public bool _lookForUpdates = true;
     [ObservableProperty]
-    private DateTime _lastManagerUpdateCheckTime = DateTime.MaxValue;
+    private DateTime _lastManagerUpdateCheckTime = DateTime.MinValue;
     [ObservableProperty]
     public string _currentLanguageSelected = CultureInfo.CurrentCulture.Name;
     [ObservableProperty]
