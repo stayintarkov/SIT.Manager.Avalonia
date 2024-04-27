d9c6917ab0fa9afb236e95af199bd83ec100c534
Luna <34936608+Plootie@users.noreply.github.com>
2024-04-27 19:15:46 +0100

    Potentially fix deserializer stoopidness

--- SIT.Manager/Models/Aki/AkiCharacter.cs
+++ SIT.Manager/Models/Aki/AkiCharacter.cs
@@ -2,16 +2,33 @@
 using System.Text.Json.Serialization;
 
 namespace SIT.Manager.Models.Aki;
-public class AkiCharacter(AkiServer parent, string username, string password) : ObservableObject
+public class AkiCharacter : ObservableObject
 {
     [JsonPropertyName("Username")]
-    public string Username { get; init; } = username;
+    public string Username { get; init; }
     [JsonPropertyName("Password")]
-    public string Password { get; init; } = password;
+    public string Password { get; init; }
     [JsonPropertyName("ProfileID")]
     public string ProfileID { get; internal set; } = "";
     [JsonPropertyName("Edition")]
     public string Edition { get; internal set; } = "Edge of Darkness";
     [JsonIgnore]
-    public AkiServer ParentServer = parent;
+    public AkiServer ParentServer;
+
+    public AkiCharacter(AkiServer parent, string username, string password)
+    {
+        ParentServer = parent;
+        Username = username;
+        Password = password;
+    }
+
+    [JsonConstructor]
+    AkiCharacter(AkiServer parent, string username, string password, string edition, string profileID)
+    {
+        this.ParentServer = parent;
+        this.Username = username;
+        this.Password = password;
+        this.Edition = edition;
+        this.ProfileID = profileID;
+    }
 }
