525ff26c189a481ddab41e89b913acea7477adf3
Luna <34936608+Plootie@users.noreply.github.com>
2024-05-01 15:01:03 +0100

    I might have the stupid

---
+++ SIT.Manager/Views/Tools/GeneralToolsView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using SIT.Manager.Controls;
+using SIT.Manager.ViewModels.Tools;
+
+namespace SIT.Manager.Views.Tools;
+public partial class GeneralToolsView : UserControl
+{
+    public GeneralToolsView()
+    {
+        InitializeComponent();
+        this.DataContext = App.Current.Services.GetService<GeneralToolsViewModel>();
+    }
+}
