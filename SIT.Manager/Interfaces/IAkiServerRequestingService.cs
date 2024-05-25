edb13123ad01fad219fd690d70311c18851eae82
Luna <34936608+Plootie@users.noreply.github.com>
2024-05-25 02:30:00 +0100

    Small refactor of requesting service

--- SIT.Manager/Interfaces/IAkiServerRequestingService.cs
+++ SIT.Manager/Interfaces/IAkiServerRequestingService.cs
@@ -10,7 +10,7 @@ namespace SIT.Manager.Interfaces;
 public interface IAkiServerRequestingService
 {
     public Task<int> GetPingAsync(AkiServer akiServer, CancellationToken cancellationToken = default);
-    public Task<AkiServer> GetAkiServerAsync(Uri serverAddresss, bool fetchInformation = true, CancellationToken cancellationToken = default);
+    public Task<AkiServer> GetAkiServerAsync(Uri serverAddress, bool fetchInformation = true, CancellationToken cancellationToken = default);
     public Task<List<AkiMiniProfile>> GetMiniProfilesAsync(AkiServer server, CancellationToken cancellationToken = default);
     public Task<(string, AkiLoginStatus)> LoginAsync(AkiServer server, AkiCharacter character, CancellationToken cancellationToken = default);
     public Task<(string, AkiLoginStatus)> RegisterCharacterAsync(AkiServer server, AkiCharacter character, CancellationToken cancellationToken = default);
