b44a04b12e033e2654ed0266335d4ea1f3b3b5cc
Luna <34936608+Plootie@users.noreply.github.com>
2024-07-06 17:22:36 +0100

    Overhaul DownloadAsync

--- SIT.Manager/Extentions/HttpClientExtentions.cs
+++ SIT.Manager/Extentions/HttpClientExtentions.cs
@@ -11,25 +11,22 @@ namespace SIT.Manager.Extentions;
 /// </summary>
 internal static class HttpClientExtentions
 {
-    public static async Task DownloadAsync(this HttpClient client, Stream destination, string url, IProgress<double> progressReporter, CancellationToken cancellationToken = default)
+    public static async Task DownloadAsync(this HttpClient client, Stream destination, string url, IProgress<double>? progressReporter, CancellationToken cancellationToken = default)
     {
         using HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
         response.EnsureSuccessStatusCode();
-        using Stream contentStream = await response.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
+        await using Stream contentStream = await response.Content.ReadAsStreamAsync(cancellationToken).ConfigureAwait(false);
         long? contentLength = response.Content.Headers.ContentLength;
-        if (!contentLength.HasValue)
+        if (contentLength.HasValue)
         {
-            await contentStream.CopyToAsync(destination, cancellationToken).ConfigureAwait(false);
+            double totalLength = contentLength.Value;
+            Progress<long> reportWrapper = new(currentValue => progressReporter?.Report(currentValue / totalLength * 100));
+            await contentStream.CopyToAsync(destination, 65535, reportWrapper, cancellationToken).ConfigureAwait(false);
         }
         else
         {
-            double totalLength = contentLength.Value;
-            Progress<long> reportWrapper = new(br =>
-            {
-                double currentValue = br;
-                progressReporter.Report(currentValue / totalLength * 100);
-            });
-            await contentStream.CopyToAsync(destination, 65535, reportWrapper, cancellationToken).ConfigureAwait(false);
+            //The server didn't tell us how big the response is, so we can't report any progress
+            await contentStream.CopyToAsync(destination, cancellationToken).ConfigureAwait(false);
         }
     }
 }
